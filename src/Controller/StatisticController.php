da60394be8da05870ca17bdc86b7df4d66603bcf
kentoje <k.monthubert@gmail.com>
2021-02-17 13:49:28 +0100

    add data in statistic

---
+++ src/Controller/StatisticController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ClientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StatisticController extends AbstractController
+{
+    /**
+     * @Route("/statistic", name="statistic")
+     * @param ClientRepository $clientRepository
+     * @return Response
+     */
+    public function index(ClientRepository $clientRepository): Response
+    {
+        $clients = $clientRepository->findAllOrderByBeerDesc();
+
+        return $this->render('statistic/index.html.twig', [
+            'data' => [
+                'clients' => $clients,
+                'count' => count($clients),
+                'average' => $clientRepository->averageBeerPerClient(),
+                'stdDeviation' => $clientRepository->findStandardDeviationFromNumBeer(),
+            ],
+        ]);
+    }
+}
--- src/Repository/ClientRepository.php
+++ src/Repository/ClientRepository.php
@@ -33,4 +33,48 @@ class ClientRepository extends ServiceEntityRepository
 
         return $result[0];
     }
+
+    /**
+     * @return float Returns float average.
+    */
+    public function averageBeerPerClient(): float
+    {
+        [$data] = $this
+            ->createQueryBuilder('b')
+            ->select('SUM(b.number_beer) / COUNT(b) AS average')
+            ->getQuery()
+            ->getResult()
+        ;
+
+        return (float) $data['average'];
+    }
+
+    /**
+     * @return Client[] Returns clients order by number of beer desc.
+    */
+    public function findAllOrderByBeerDesc(): array
+    {
+        return $this
+            ->createQueryBuilder('c')
+            ->orderBy('c.number_beer', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * STD() SQL Function returns the standard deviation ("écart type")
+     * @return float Returns float average.
+    */
+    public function findStandardDeviationFromNumBeer(): float
+    {
+        [$data] = $this
+            ->createQueryBuilder('c')
+            ->select('STD(c.number_beer) AS deviation')
+            ->getQuery()
+            ->getResult()
+        ;
+
+        return $data['deviation'];
+    }
 }
