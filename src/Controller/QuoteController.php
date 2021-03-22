80c3183aa78c860333d3f5830561c5c2725eada6
kentoje <k.monthubert@gmail.com>
2021-03-22 16:59:06 +0100

    add form

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Quote;
 use App\Repository\QuoteRepository;
 use App\Services\HelperParserService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,6 +31,45 @@ class QuoteController extends AbstractController
                 $quotes,
                 ['title', 'content'],
             ),
+        ]);
+    }
+
+    /**
+     * @Route("/quote/new", name="quote_new", methods={"GET", "POST"})
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param Quote|null $quote
+     * @return Response
+     */
+    public function new(Request $request, EntityManagerInterface $manager, Quote $quote = null): Response
+    {
+        if (!$quote) {
+            $quote = new Quote();
+        }
+
+        $form = $this
+            ->createFormBuilder($quote)
+            ->add('title', TextType::class, ['required' => true])
+            ->add('content', TextareaType::class)
+            ->add(
+                'save',
+                SubmitType::class, ['label' => 'Create Quote'],
+            )
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($quote);
+            $manager->flush();
+
+            return $this->redirectToRoute('quotes');
+        }
+
+        return $this->render('quote/new.html.twig', [
+            'quote' => $quote,
+            'form' => $form->createView(),
         ]);
     }
 }
